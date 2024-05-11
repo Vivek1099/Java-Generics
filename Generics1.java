dda881454f7e66c95ba3660c0f9a0ab3d2a3445c
Vivek Sawaiyan <150422660+Vivek1099@users.noreply.github.com>
2024-05-11 10:22:21 +0530

    Update Generics1.java

--- Generics1.java
+++ Generics1.java
@@ -1,11 +1,13 @@
 package practice;
 
-interface base<t>
+//Implementation of generics using interface.
+
+interface base<t> //generics type interface
 {
 	void print(t x);
 }
 
-class derived<t> implements base<t>
+class derived<t> implements base<t> //implementation of generics
 {
 	public void print(t x)
 	{
@@ -17,6 +19,7 @@ public class Generics1
 {
 	public static void main(String args[])
 	{
+		//passing of arguements.
 		derived d=new derived();
 		d.print(45);
 		d.print(39.849);
